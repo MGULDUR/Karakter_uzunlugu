ebbef13caac72292b2fcaf5c8d6a73353d35ef3d
Bilge Adam <bilge.adam@baogrenci.com>
2021-09-28 15:21:10 +0300

    Add project files.

---
+++ deneme01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace deneme01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string isim="mustafa";
+
+
+            Console.WriteLine(Encoding.Unicode.GetByteCount(isim));
+
+        }
+    }
+}
